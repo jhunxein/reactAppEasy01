10dccce216cb1352627a446aa2c5ddff74adcd71
jhunxein <jhunsensamonte@gmail.com>
2021-03-23 21:31:57 -0800

    React Easy 01 Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
+
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+import logo from './images/logo.svg';
+import mockups from './images/illustration-mockups.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const refContainer = useRef(null);
+
+	function handleResize() {
+		const height = window.innerHeight;
+		refContainer.current.style.height = `${height}px`;
+	}
+
+	useEffect(() => {
+		handleResize();
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	return (
+		<div className="container" ref={refContainer}>
+			<header>
+				<a href="/" className="logo">
+					<img src={logo} alt="..." />
+				</a>
+			</header>
+			<main className="grid">
+				<div className="left">
+					<img src={mockups} alt="..." className="mockup" />
+				</div>
+				<div className="rigth">
+					<h1>build the community your fans will love</h1>
+					<p>
+						Huddle re-images the way we build communities. You have a voice, but so
+						does your audience. Create connections with your users as you engage in
+						genuine discussions.
+					</p>
+					<button type="button" className="cta">
+						register
+					</button>
+					<div className="social-icons">
+						<div className="wrapper">
+							<a href="https://www.facebook.com" title="Visit our facebook page">
+								<FaFacebook />
+							</a>
+							<a
+								href="https://www.twitter.com"
+								title="Take a look on our latest tweets"
+							>
+								<FaTwitter />
+							</a>
+							<a href="https://instagram.com" title="Be updated in our instagram post">
+								<FaInstagram />
+							</a>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
